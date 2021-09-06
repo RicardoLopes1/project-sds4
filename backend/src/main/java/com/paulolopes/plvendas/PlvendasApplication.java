88489ab4236e9ba76a49e615221d6ed7daa5088d
Ricardo Lopes <prslopes1@gmail.com>
2021-09-06 13:24:48 -0300

    Project created

---
+++ backend/src/main/java/com/paulolopes/plvendas/PlvendasApplication.java
@@ -0,0 +1,13 @@
+package com.paulolopes.plvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/paulolopes/plvendas/PlvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulolopes.plvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
